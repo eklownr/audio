ea3c7b803b0f42bf8a49653f2814175ec322c57b
eklownr <eklownr@gmail.com>
2024-10-03 19:46:38 +0200

    testing soundfile

--- guitar_tuner/guitar_tuner.py
+++ guitar_tuner/guitar_tuner.py
@@ -1,4 +1,7 @@
-import pyaudio
+import threading
 
-version_text = pyaudio.get_portaudio_version_text()
-print(version_text)
+import sounddevice as sd
+import soundfile as sf
+
+data, samplerate = sf.read('../sounds/e2.mp3')
+sf.write('new_fileE2.flac', data, samplerate)
